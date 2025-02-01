0ae1b844d0ed9769006b04d2233c749219847c3d
Javy-Joshua <javyjoshua@gmail.com>
2025-02-01 01:39:16 +0100

    first commit

---
+++ api.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const app =  express()
+const Henny = require('./db')
+
+const port = 3000
+
+
+app.get("/home", (req, res) => {
+    console.log("we home")
+})
+
+app.get("/api", (req, res) => {
+    const currentDateTime = new Date().toISOString()
+
+    const details = {
+      email: Henny.Henny.registeredEmail,
+      timestamp: currentDateTime,
+      githubUrl: Henny.Henny.githubUrl,
+    };
+
+    
+    console.log(details);
+
+    return res.status(200).json({
+        message: "details created",
+        details 
+    })
+
+})
+
+
+app.get('*', (req, res) => {
+    return res.status(404).json({
+        data: null,
+        error: "Route not found"
+    })
+})
+
+
+
+app.listen(port, ()=> { 
+    console.log(`We are listening to port:${port}`)
+})
---
+++ db.js
@@ -0,0 +1,12 @@
+// db.js
+
+
+const Henny = {
+     registeredEmail : "javyjoshua@gmail.com",
+     githubUrl : "https://github.com/Javy-Joshua/HNG-Stage0"
+}
+
+module.exports = {
+  Henny
+};
+
